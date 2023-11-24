72b5032fb4694857db1eac722bb81334f86b08f1
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-24 19:23:22 -0300

    Criar o botão para adicionar novos gastos

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import Presupuesto from "./components/Presupuesto.vue";
 import ControlPresupuesto from "./components/ControlPresupuesto.vue";
+import iconoNuevoGasto from "./assets/img/nuevo-gasto.svg";
 
 const presupuesto = ref(0);
 const disponible = ref(0);
@@ -29,6 +30,12 @@ const definirPresupuesto = (cantidad) => {
         />
       </div>
     </header>
+
+    <main v-if="presupuesto > 0">
+      <div class="crear-gasto">
+        <img :src="iconoNuevoGasto" alt="icono nuevo gasto" />
+      </div>
+    </main>
   </div>
 </template>
 
@@ -94,5 +101,16 @@ header h1 {
   background-color: var(--blanco);
   border-radius: 1.2rem;
   padding: 5rem;
+}
+
+.crear-gasto {
+  position: fixed;
+  bottom: 5rem;
+  right: 5rem;
+}
+
+.crear-gasto img {
+  width: 5rem;
+  cursor: pointer;
 }
 </style>
