07caec1d6cc10cba42318c0f0e9b78a27fa49436
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-24 22:30:14 -0300

    Adicionar CSS ao formulário de Modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -11,7 +11,7 @@ const emit = defineEmits(["ocultar-modal"]);
     </div>
 
     <div class="contenedor">
-      <form>
+      <form class="nuevo-gasto">
         <legend>Añadir Gastos</legend>
         <div class="campo">
           <label for="nombre">Nombre Gasto:</label>
@@ -67,6 +67,44 @@ const emit = defineEmits(["ocultar-modal"]);
 }
 .cerrar-modal img {
   width: 3rem;
+  cursor: pointer;
+}
+.nuevo-gasto {
+  margin: 10rem auto 0 auto;
+  display: grid;
+  gap: 2rem;
+}
+
+.nuevo-gasto legend {
+  text-align: center;
+  color: var(--blanco);
+  font-size: 3rem;
+  font-weight: 700;
+}
+
+.campo {
+  display: grid;
+  gap: 2rem;
+}
+
+.nuevo-gasto input,
+.nuevo-gasto select {
+  background-color: var(--gris-claro);
+  border-radius: 1rem;
+  padding: 1rem;
+  border: none;
+  font-size: 2.2rem;
+}
+
+.nuevo-gasto label {
+  color: var(--blanco);
+  font-size: 3rem;
+}
+
+.nuevo-gasto input[type="submit"] {
+  background-color: var(--azul);
+  color: var(--blanco);
+  font-weight: 700;
   cursor: pointer;
 }
 </style>
