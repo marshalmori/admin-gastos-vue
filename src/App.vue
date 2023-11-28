9e8ba9ccfbea321ffa4ae390f34adc0dff4d48d4
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-28 10:09:46 -0300

    Aplicar CSS ao gastos

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Presupuesto from "./components/Presupuesto.vue";
 import ControlPresupuesto from "./components/ControlPresupuesto.vue";
 import iconoNuevoGasto from "./assets/img/nuevo-gasto.svg";
 import Modal from "./components/Modal.vue";
+import Gasto from "./components/Gasto.vue";
 import { generarId } from "./helpers";
 
 const modal = reactive({
@@ -81,6 +82,11 @@ const guardarGasto = () => {
     </header>
 
     <main v-if="presupuesto > 0">
+      <div class="listado-gastos contenedor">
+        <h2>{{ gastos.length > 0 ? "Gastos" : "No hay gastos" }}</h2>
+
+        <Gasto v-for="gasto in gastos" :key="gasto.id" :gasto="gasto" />
+      </div>
       <div class="crear-gasto">
         <img
           :src="iconoNuevoGasto"
@@ -175,5 +181,14 @@ header h1 {
 .crear-gasto img {
   width: 5rem;
   cursor: pointer;
+}
+
+.listado-gastos {
+  margin-top: 12rem;
+}
+
+.listado-gastos h2 {
+  font-weight: 900;
+  color: var(--gris-oscuro);
 }
 </style>
