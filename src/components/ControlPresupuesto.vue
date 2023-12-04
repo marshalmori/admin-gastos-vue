c54aa010e2ecf53d7c03d9db921ac8c3ea9906e7
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-12-04 15:21:17 -0300

    Mostrar o valor da porcentagem de gasto no centro do gráfico de circulos

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { computed } from "vue";
 import { formatearCantidad } from "../helpers";
 import CircleProgress from "vue3-circle-progress";
 import "vue3-circle-progress/dist/circle-progress.css";
@@ -19,13 +20,20 @@ const props = defineProps({
 });
 
 const emit = defineEmits(["definir-disponible", "reset-app"]);
+
+const porcentaje = computed(() => {
+  return parseInt(
+    ((props.presupuesto - props.disponible) / props.presupuesto) * 100
+  );
+});
 </script>
 
 <template>
   <div class="dos-columnas">
     <div class="contenedor-grafico">
+      <p class="porcentaje">{{ porcentaje }}%</p>
       <CircleProgress
-        :percent="50"
+        :percent="porcentaje"
         :size="250"
         :border-width="30"
         :border-bg-width="30"
@@ -45,6 +53,22 @@ const emit = defineEmits(["definir-disponible", "reset-app"]);
 </template>
 
 <style scoped>
+.contenedor-grafico {
+  position: relative;
+}
+
+.porcentaje {
+  position: absolute;
+  margin: auto;
+  top: calc(50% - 1.5rem);
+  left: 0;
+  right: 0;
+  text-align: center;
+  z-index: 100;
+  font-size: 3rem;
+  font-weight: 900;
+  color: var(--gris-oscuro);
+}
 .dos-columnas {
   display: flex;
   flex-direction: column;
